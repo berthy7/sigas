eddb0a0f1b04a778e622a7714ea6d755d66e370f
Berthy Vargas V <bvargas@cloudbit.local>
2020-06-16 15:24:35 -0400

    actualizacion tipo taxi

--- server/condominios/movimiento/assets/movimiento.js
+++ server/condominios/movimiento/assets/movimiento.js
@@ -59,6 +59,7 @@ function verificar_qr() {
 
                     $('#fkautorizacion').val(1)
                     $('#fkautorizacion').selectpicker('refresh')
+                    cargar_nropase($( "#fktipopase option:selected" ).text())
 
 
                     document.getElementById("imagen_mensaje").src = response.message;
@@ -584,6 +585,39 @@ $('#fkvehiculo').change(function () {
         $('#fkmodelo').selectpicker('refresh')
     }
 });
+
+function cargar_nropase(tipopase) {
+
+        obj = JSON.stringify({
+        'tipopase': tipopase,
+        '_xsrf': getCookie("_xsrf")
+    })
+
+    ruta = "nropase_listar_tipo";
+    //data.append('object', obj)
+    //data.append('_xsrf',getCookie("_xsrf"))
+
+    $.ajax({
+        method: "POST",
+        url: ruta,
+        data: {_xsrf: getCookie("_xsrf"), object: obj},
+        async: false
+    }).done(function (response) {
+        response = JSON.parse(response)
+
+        $('#nropase').html('');
+        var select = document.getElementById("nropase")
+        for (var i = 0; i < Object.keys(response.response).length; i++) {
+            var option = document.createElement("OPTION");
+            option.innerHTML = response['response'][i]['numero'] +" - "+response['response'][i]['tipo'];
+            option.value = response['response'][i]['id'];
+            select.appendChild(option);
+        }
+        $('#nropase').selectpicker('refresh');
+
+    })
+
+}
 
 $('#fktipopase').change(function () {
     
