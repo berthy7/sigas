1066f21189b8f5dd15b2a26139ea053d3766a0c6
Berthy Vargas V <bvargas@cloudbit.local>
2020-06-23 17:13:33 -0400

    actualizacion color gris

--- server/condominios/movimiento/assets/movimiento.js
+++ server/condominios/movimiento/assets/movimiento.js
@@ -702,6 +702,8 @@ function cargar_vehiculo(id) {
     }).done(function (response) {
         response = JSON.parse(response)
 
+        console.log(response)
+
         // $('#cantpasajeros').val(response.response.cantpasajeros)
         $('#placa').val(response.response.placa)
         $('#tipo').val(response.response.tipo)
